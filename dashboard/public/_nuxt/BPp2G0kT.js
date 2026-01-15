ac5a8931a6d70b287577417ac0c28c62214e4f61
wnlen <544241974@qq.com>
2026-01-15 21:23:41 +0800

    ui

--- dashboard/public/_nuxt/BPp2G0kT.js
+++
@@ -1,1 +0,0 @@
-import{M as e,yt as t}from"./B99QpFu8.js";import{t as n}from"./BYCvG7bQ.js";const r=e(`logs`,()=>{let e=n(),r=t([]),i=t(!1),a=1;return{logs:r,paused:i,addLog:t=>{i.value||(r.value=[{...t,seq:a},...r.value].slice(0,e.logMaxRows),a++)},clearLogs:()=>{r.value=[],a=1},togglePaused:()=>{i.value=!i.value}}});export{r as t};
